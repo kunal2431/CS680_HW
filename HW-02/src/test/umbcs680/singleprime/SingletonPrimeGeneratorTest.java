352d961d69fdbd13d723470297fd850793f0f702
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-03-07 19:47:05 -0500

    Code review changes SingletonPrimeGeneratorTest.java

--- HW-02/src/test/umbcs680/singleprime/SingletonPrimeGeneratorTest.java
+++ HW-02/src/test/umbcs680/singleprime/SingletonPrimeGeneratorTest.java
@@ -4,14 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import java.util.Iterator;
 import java.util.LinkedList;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeAll;
 
 public class SingletonPrimeGeneratorTest{
+    private static SingletonPrimeGenerator cut;
+
+    @BeforeAll
+    public static void setUp(){
+        cut = SingletonPrimeGenerator.getInstance(1, 1000);
+    }
+    
     @Test
     //TestCase1: Verify 1) getInstance() return non-null value 2) getInstance() returns the identical instance when it is called multiple times.
     //Structural test: Verify Singleton functionality
     public void SingletonPrimeGenerate_getInstance(){
-        SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(1, 5);
+        cut.setRange(1,5);
         assertNotNull(cut);
         SingletonPrimeGenerator cut1 = SingletonPrimeGenerator.getInstance(1, 5);
         assertSame(cut, cut1);
@@ -22,7 +29,6 @@ public class SingletonPrimeGeneratorTest{
     //Functional test: Verify getPrimes() returns the expected result
     public void SingletonPrimeGenerate1to15(){
         Long[] expected = {2L, 3L, 5L, 7L, 11L, 13L};
-        SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(1, 15);
         cut.setRange(1, 15);
         cut.generatePrimes();
         assertTrue(cut instanceof SingletonPrimeGenerator);
@@ -33,7 +39,6 @@ public class SingletonPrimeGeneratorTest{
     //TestCase3: Test isPrime() method - Positive Test
     //Structural test: Check isPrime() method checks prime numbers correctly
     public void SingletonPrimeGenerate_isPrime(){
-        SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(1, 1000);
         cut.setRange(1, 1000);
         assertTrue(cut.isPrime(79L));
         assertTrue(cut.isPrime(157L));
@@ -48,7 +53,6 @@ public class SingletonPrimeGeneratorTest{
     //TestCase4: Test Prime Generated from 7 to 15 are Prime Numbers only - Positive Test
     //Functional test: Check prime generator produces prime numbers only using isPrime() method which is working as expected as tested in test case 3
     public void SingletonPrimeGenerate7to15(){
-        SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(7, 15);
         cut.setRange(7, 15);
         cut.generatePrimes();
         assertNotNull(cut.getPrimes().toArray());
@@ -64,7 +68,6 @@ public class SingletonPrimeGeneratorTest{
     //Structural test: check condition - if(from >= 1 && to > from) works as expected and throws RuntimeException
     public void SingletonPrimeGenerate6to1(){
         try{
-            SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(6, 1);
             cut.setRange(6, 1);
             fail("From is Greater than to");
         }
@@ -79,7 +82,6 @@ public class SingletonPrimeGeneratorTest{
     //Structural test: check condition - if(from >= 1 && to > from) works as expected and throws RuntimeException
     public void SingletonPrimeGenerateNeg5to8(){
         try{
-            SingletonPrimeGenerator cut = SingletonPrimeGenerator.getInstance(-5, 8);
             cut.setRange(-5, 8);
             fail("From is less than 1");
         }
