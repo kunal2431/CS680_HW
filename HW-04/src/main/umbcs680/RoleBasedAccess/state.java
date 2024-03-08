666b720eb07e863624357ecffc1eeee1f594aafb
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-03-07 22:28:35 -0500

    Delete HW-04/src directory

--- HW-04/src/main/umbcs680/RoleBasedAccess/state.java
+++
@@ -1,9 +0,0 @@
-package umbcs680.RoleBasedAccess;
-
-import java.nio.file.AccessDeniedException;
-
-public interface state{
-	public void getGuestPage() throws AccessDeniedException;
-	public void getPremiumPage() throws AccessDeniedException;
-	public void getAdminPage() throws AccessDeniedException;
-}
