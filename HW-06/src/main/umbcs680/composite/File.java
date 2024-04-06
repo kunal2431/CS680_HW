22fab16a2272d187f716b5f65b1cac888e6c6756
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-04-06 02:22:57 -0400

    Add files via upload

---
+++ HW-06/src/main/umbcs680/composite/File.java
@@ -0,0 +1,15 @@
+package umbcs680.composite;
+
+import java.time.LocalDateTime;
+
+public class File extends FSElement{
+
+    public File(Directory parent, String name, int size, LocalDateTime creationTime){
+        super(parent, name, size, creationTime);
+    }
+
+    public boolean isDirectory() {
+        return false;
+    }
+
+}
