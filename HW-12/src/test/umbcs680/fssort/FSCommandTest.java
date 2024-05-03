8cc36091dbd069d9027917f73957969ee0008c26
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-05-03 13:10:13 -0400

    Minor Change FSCommandTest.java

--- HW-12/src/test/umbcs680/fssort/FSCommandTest.java
+++ HW-12/src/test/umbcs680/fssort/FSCommandTest.java
@@ -29,7 +29,7 @@ public class FSCommandTest{
     public static void setUpFS(){
         fs = TestFixtureInitializer.createFS();
         repo = fs.getRootDirs().getFirst();
-        src = repo.getSubDirectories().get(1);
+        src = repo.getSubDirectories().getFirst();
         main = src.getSubDirectories().getFirst();
         test = src.getSubDirectories().get(1);
         readme = repo.getFiles().getFirst();
